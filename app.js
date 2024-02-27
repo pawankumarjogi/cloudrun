7de3d1efe6256b90a4e233fcb4b95b3279fbd7e0
pawankumarjogi <pavan.jogi@niveussolutions.com>
2024-02-27 10:56:31 +0530

    modified app

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+
 const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app = express(); 
+  
+app.use(express.urlencoded({ extended: true })) 
+app.use(express.json()) 
+  
+app.get('/',  
+   (req, res) => res.send('Dockerizing Node Application')) 
+  
+app.listen(5000,  
+   () => console.log(`⚡️[bootup]: Server is running at port: 5000`));
