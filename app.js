d48680cd1192e0603c934c6acdb6bed090659dcc
pawankumarjogi <pavan.jogi@niveussolutions.com>
2024-04-23 10:47:09 +0530

    revision commit

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+const express = require("express");
+app = express();
 
-const express = require('express')
-app = express(); 
-  
-app.use(express.urlencoded({ extended: true })) 
-app.use(express.json()) 
-  
-app.get('/',  
-   (req, res) => res.send('Dockerizing Node Application')) 
-  
-app.listen(5000,  
-   () => console.log(`⚡️[bootup]: Server is running at port: 5000`));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) =>
+  res.send("Dockerizing Node Application version docker for revisioning")
+);
+
+app.listen(5000, () =>
+  console.log(`version 2 ⚡️[bootup]: Server is running at port: 5000`)
+);
